acfc205ca3a936ead4323d7febfaa034e0049a63
Amiya-24 <flashfazz@gmail.com>
2025-10-29 09:57:34 +0800

    Create delete.php

---
+++ delete.php
@@ -0,0 +1,27 @@
+<?php
+session_start();
+require 'config/database.php';
+require 'src/functions.php'; // Panggil functions.php
+
+// Ambil ID dari URL
+$id = isset($_GET['id']) ? (int)$_GET['id'] : 0;
+
+if ($id == 0) {
+    set_message("ID tidak valid.", "error");
+    redirect('index.php');
+}
+
+try {
+    // Ganti nama tabel ke 'barang'
+    $sql = "DELETE FROM barang WHERE id = ?";
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute([$id]);
+
+    set_message("Data berhasil dihapus!", "sukses");
+    redirect("index.php");
+
+} catch (PDOException $e) {
+    set_message("Gagal menghapus data. Data ini mungkin terkait dengan data lain.", "error");
+    redirect("index.php");
+}
+?>
